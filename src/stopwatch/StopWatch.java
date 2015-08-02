124aa8efbae7b84370d17982afbf42f8c0bf87c9
Jackson <jackjer2004@hotmail.com>
2015-08-03 11:25:02 +1200

    Commit-8: Stop Watch Frame

--- src/stopwatch/StopWatch.java
+++ src/stopwatch/StopWatch.java
@@ -1,3 +1,30 @@
 package stopwatch;
 
-/// Attempt to test commit
+/// Stop watch Frame
+
+public void launchStopWatch()
+	{
+		topPanel = new JPanel();
+		topPanel.setBackground(Color.orange);
+		bottomPanel = new JPanel();
+		bottomPanel.setBackground(Color.yellow);
+		topPanel.add(timeLbl);
+		bottomPanel.add(startBtn);
+		bottomPanel.add(stopBtn);
+		bottomPanel.add(resetBtn);
+
+		this.setLayout(new BorderLayout());
+
+		add(topPanel, BorderLayout.CENTER);
+		add(bottomPanel, BorderLayout.SOUTH);
+
+		setSize(300,200);
+		setBackground(Color.orange);
+
+	}//end of launchClock
+
+	public static void main(String[] args)
+	{
+		MyTestFrame myTestFrame1 = new MyTestFrame();
+	}
+
